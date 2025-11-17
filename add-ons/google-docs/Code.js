65cb422f19e9cdc104515e59bdcb54cdf99eb291
Robert Hill <robert.hill@uphillsolutions.tech>
2025-11-17 12:54:53 -0500

    refactor: simplify Google Docs add-on menu creation

--- add-ons/google-docs/Code.js
+++ add-ons/google-docs/Code.js
@@ -1,22 +1,14 @@
 // add-ons/google-docs/Code.js
 
-// TODO: For published add-ons, use createAddonMenu() instead of createMenu()
-// See README.md for details on menu-based Editor add-on best practices
-
 /**
  * Main function to create the add-on menu.
  * This runs when the document is opened or the add-on is installed.
  * @param {object} e - The event object.
  */
-function onOpen(e) {
+function onOpen(_e) {
   const ui = DocumentApp.getUi();
-  const menu = ui.createAddonMenu("UpHill Solutions Add Ons");
-
-  if (e && e.authMode == ScriptApp.AuthMode.NONE) {
-    menu.addItem("Start & Authorize", "showAuthPrompt");
-  } else {
-    buildFullMenu(menu);
-  }
+  const menu = ui.createMenu("UpHill Solutions Add Ons");
+  buildFullMenu(menu);
   menu.addToUi();
 }
 
@@ -47,24 +39,4 @@ function buildFullMenu(menu) {
           "calculateAndUpdateWithDynamicRate",
         ),
     );
-}
-
-/**
- * Shows a simple prompt to trigger the authorization flow.
- */
-function showAuthPrompt() {
-  const ui = DocumentApp.getUi();
-  const response = ui.alert(
-    "Authorization Required",
-    "This add-on needs your permission to run. Please click OK to authorize, then run the 'Start' menu again.",
-    ui.ButtonSet.OK,
-  );
-
-  try {
-    DocumentApp.getActiveDocument();
-  } catch (e) {
-    ui.alert(
-      "Authorization may not have completed. Please try running 'Start & Authorize' again.",
-    );
-  }
 }
